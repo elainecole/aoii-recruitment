0f6eeec94cb43f5778bdbdee8f0b95a80bc1c156
Daniel Borstelmann <dborstelmann@wustl.edu>
2016-11-18 14:52:19 -0600

    fixes

---
+++ api/migrations/0002_account_email.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('api', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='account',
+            name='email',
+            field=models.CharField(default='', unique=True, max_length=63),
+            preserve_default=False,
+        ),
+    ]

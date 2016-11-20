561183d47be1e5acfcdf984aa24ee48fd0f7dd52
Elaine Cole <elainemcole@wustl.edu>
2016-11-20 12:00:59 -0600

    addition of user_base to hello.js

--- webapp/static/js/hello.js
+++ webapp/static/js/hello.js
@@ -3,15 +3,21 @@ hk = hk || {};
 hk.HelloView = BB.View.extend({
     el: '#hello',
     template: _.template($('#hello-template').html()),
+    userTemplate: _.template($('#user-template').html()),
 
     initialize: function (options) {
+
+        this.model = new hk.HelloUserModel();
         this.render();
+
+        this.listenTo(this.model, 'sync', this.renderUser)
     },
 
     render: function () {
         this.$el.empty().append(this.template());
     },
 
-    events: {
-    },
+    renderUser: function () {
+        this.$('.user-holder').empty().append(this.userTemplate());
+    }
 });
